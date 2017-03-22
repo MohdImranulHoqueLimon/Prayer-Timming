e7f31416c1dfd670a82a1bee692c47828940d268
limon <limon1036@hotmail.com>
2017-03-22 21:33:18 +0600

    add to-do list for the project

---
+++ app/src/main/java/com/limon/PrayerTiming/utility/ToDo.java
@@ -0,0 +1,24 @@
+package com.limon.PrayerTiming.utility;
+
+/**
+ * Created by Limon on 3/22/2017.
+ */
+
+public class ToDo {
+    //Should check if gps permission is enable if not show settings to enable gps this
+    //works for more that android lolipop version
+
+    //remove all logs from project
+
+    //remove all unused image and layouts
+
+    //minimize the size of mp3 files
+
+    // check the app can't crash
+
+    //try to upload with old version with same package name and certificate key
+
+    //check compass with latest mobile
+
+    //try to avoid using deprecated functions
+}
